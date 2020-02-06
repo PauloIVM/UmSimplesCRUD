2541a8d13c8d926664ddaba7a916cebb8837e70f
PauloIVM <pakas_vieira_95@hotmail.com>
2020-02-06 11:13:53 -0300

    Enviando projeto ao GitHub, CREATE e READ implementados

---
+++ routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+
+
+// Rotas da home
+route.get('/', homeController.index);
+
+//Rotas de Login
+route.get('/loginRota/index', loginController.index)
+route.get('/loginRota/logout', loginController.logout)
+route.post('/login/register', loginController.register)
+route.post('/login/login', loginController.login)
+module.exports = route;
---
+++ src/controllers/loginController.js
@@ -0,0 +1,57 @@
+const Login = require('../models/LoginModel')
+
+module.exports.index = (req, res) => {
+    console.log(req.session.user)
+    if(req.session.user) return res.redirect('/')
+    res.render('login')
+}
+
+module.exports.register = async function(req, res){
+    //Esses paranauês com o login aqui e no LoginModel é simplesmente para a validação ficar no model, e 
+    //não no controllers
+    try{
+        const login = new Login(req.body)
+        await login.register()
+    
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors)
+            req.session.save(() => res.redirect('back'))
+            return
+        }
+
+        req.flash('success', 'Usuário criado com sucesso')
+        req.session.save(() => res.redirect('back'))
+        
+
+    }catch(e){
+        return res.render('404')
+    }
+    
+}
+
+module.exports.login = async function(req, res){
+    try{
+        const login = new Login(req.body)
+        await login.login()
+    
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors)
+            req.session.save(() => res.redirect('back'))
+            return
+        }
+
+        req.flash('success', 'Usuário logado com sucesso')
+        req.session.user = login.user
+        req.session.save(() => res.redirect('back'))
+        
+
+    }catch(e){
+        return res.render('404')
+    }
+    
+}
+
+module.exports.logout = function(req, res){
+    req.session.destroy()
+    res.redirect('/')
+}
